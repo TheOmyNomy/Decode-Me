0a8670bf407e75ec6760a1f4e394fd794ef95a4e
TheOmyNomy <theomynomy@gmail.com>
2017-07-02 17:28:35 +1000

    Initial commit

---
+++ DecodeMe.Decode/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DecodeMe.Decode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ascii.Decode";
+
+            Console.Write("Input: ");
+            string[] input = Console.ReadLine().Split();
+
+            string segment = string.Empty, result = string.Empty;
+
+            foreach (string ascii in input)
+            {
+                char character = (char)int.Parse(ascii);
+
+                if (character == ' ')
+                {
+                    result += reverse(segment) + ' ';
+                    segment = string.Empty;
+                }
+                else segment += character;
+            }
+
+            result += reverse(segment).TrimEnd();
+            Console.WriteLine("Result: " + result);
+        }
+
+        private static string reverse(string line)
+        {
+            char[] array = line.ToCharArray();
+            Array.Reverse(array);
+
+            return new string(array);
+        }
+    }
+}
---
+++ DecodeMe.Encode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DecodeMe.Encode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ascii.Encode";
+
+            Console.Write("Input: ");
+            string[] input = Console.ReadLine().Split();
+
+            string result = string.Empty;
+
+            foreach (string word in input)
+            {
+                string segment = string.Empty;
+                for (int i = word.Length - 1; i >= 0; i--) segment += (int)word[i] + " ";
+
+                result += segment.TrimEnd() + " 32 ";
+            }
+
+            result = result.Remove(result.Length - 4);
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
